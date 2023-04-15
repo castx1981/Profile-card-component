947db1337ce573a02985644193c895900ba7ddaa
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-15 23:12:18 +0800

    add the missing two body background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['"Kumbh Sans"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
+      },
+      backgroundImage: {
+        'pattern-card': "url('images/bg-pattern-card.svg')",
+      },
+      ringWidth: {
+        '6': '6px',
+      },
+    }
   },
   plugins: [],
 }
